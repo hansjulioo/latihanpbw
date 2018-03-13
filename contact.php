a30eef587bed5885907d2c00ba9f8fac93a6bc28
hansjulioo <37090339+hansjulioo@users.noreply.github.com>
2018-03-13 14:47:19 +0700

    push connect contact to database

--- contact.php
+++ contact.php
@@ -1,4 +1,34 @@
 <?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "contact";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+$name=$_POST['name'];
+$email=$_POST['email'];
+$subject=$_POST['subject'];
+$message=$_POST['message'];
+
+$sql = "INSERT INTO contact (Nama, Email, Subject, Message)
+VALUES ('".$name."', '".$email."', '".$subject."','".$message."')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?> 
+
+
+<?php
 $name=$_POST['name'];
 $email=$_POST['email'];
 $subject=$_POST['subject'];
